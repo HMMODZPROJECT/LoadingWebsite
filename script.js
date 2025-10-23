d141e9df0aaf2cdf79004fcd92304b1d25c9e3e6
hmmodzvip <hmvip2291@gmail.com>
2025-10-23 19:16:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,5 @@ window.addEventListener("resize", () => {
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
 });
+
+<script src="https://cdn.counter.dev/script.js" data-id="8c881ef9-1404-426a-bfd5-4d07720483fd" data-utcoffset="7"></script>
